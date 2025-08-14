61290d5594b6b136558d43cf203ff44325c83860
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-08-14 10:52:19 -0500

    adding js file
    it was more of a reference than a hyperlink

---
+++ mozzarella/mozzarella.js
@@ -0,0 +1,15 @@
+	setInterval(mozztime, 500);
+	const date = new Date("2025-08-13T12:37:00");
+	function mozztime() {
+		let dhms = [];
+		const currdate = new Date();
+		dhmsConv(dhms, currdate-date);
+		document.getElementById("timer").innerHTML = dhms[0] + " day(s), " + dhms[1] + " hours, " + dhms[2] + " minutes, and " + dhms [3] + " seconds.";
+		document.getElementById("mozzdate").innerHTML = "I last had a mozzarella stick on " + date + " Central Time.";
+	}
+	function dhmsConv(list, time) {
+		list.push(Math.floor((time)/1000/60/60/24));
+		list.push(Math.floor((time)/1000/60/60) % 24);
+		list.push(Math.floor((time)/1000/60) % 60);
+		list.push(Math.floor((time)/1000) % 60);
+	}
