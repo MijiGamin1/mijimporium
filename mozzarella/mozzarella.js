5bc755febeef8ff16cca63b4fff46f3f31cfa354
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-08-18 13:49:41 -0500

    Update mozzarella.js

--- mozzarella/mozzarella.js
+++ mozzarella/mozzarella.js
@@ -1,5 +1,5 @@
 	setInterval(mozztime, 500);
-	const date = new Date("2025-08-13T12:37:00");
+	const date = new Date("2025-08-13T13:39:00");
 	function mozztime() {
 		let dhms = [];
 		const currdate = new Date();
@@ -12,4 +12,5 @@
 		list.push(Math.floor((time)/1000/60/60) % 24);
 		list.push(Math.floor((time)/1000/60) % 60);
 		list.push(Math.floor((time)/1000) % 60);
+
 	}
