fd83c12ab2c5a5392c6b42ce8ff929e1d93c8eb7
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-09-08 09:27:36 -0500

    doesn't work anymore

--- NoAIGoogle/nag.js
+++
@@ -1,5 +0,0 @@
-function search() {
-	text = document.getElementById("searchbox").value.replaceAll(" ", "+");
-    text = text + " -no_ai";
- 	window.open("https://www.google.com/search?q=" + text);
-}
